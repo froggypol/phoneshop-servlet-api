0ba083206cc637bf7d1bf50c41bf21a0a45b827f
froggypol <poinatsitovich@gmail.com>
2019-11-01 13:10:10 +0300

    with changes in Service, Listener, Dao, tests

---
+++ src/main/java/com/es/phoneshop/CustomExceptions/CustomNoSuchElementException.java
@@ -0,0 +1,11 @@
+package CustomExceptions;
+
+import java.util.stream.Stream;
+
+public class CustomNoSuchElementException extends Exception {
+//    @Override
+//    public String getMessage(String message, Throwable err) {
+//       super(message, err);
+//
+//    }
+}
---
+++ src/test/java/com/es/phoneshop/tests/service/ServiceDaoTest.java
@@ -0,0 +1,4 @@
+package com.es.phoneshop.tests.service;
+
+public class ServiceDaoTest {
+}
